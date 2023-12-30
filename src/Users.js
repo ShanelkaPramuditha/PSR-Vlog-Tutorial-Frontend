ea163eb0c07fad518d29afce9b459893f59fc4dd
Shanelka Pramuditha <99pramuditha@gmail.com>
2023-12-30 17:50:19 +0530

    added env

--- src/Users.js
+++ src/Users.js
@@ -15,7 +15,7 @@ const Users = () => {
    }, []);
 
    const getUsers = () => {
-      Axios.get('http://localhost:3001/api/users')
+      Axios.get(process.env.REACT_APP_ENDPOINT + '/api/users')
          .then(response => {
             setUsers(response.data?.response || []);
          })
@@ -30,7 +30,7 @@ const Users = () => {
          id: data.id,
          name: data.name
       }
-      Axios.post('http://localhost:3001/api/adduser', payload)
+      Axios.post(process.env.REACT_APP_ENDPOINT +  '/api/adduser', payload)
          .then(response => {
             getUsers();
             setSubmitted(false);
@@ -48,7 +48,7 @@ const Users = () => {
          name: data.name
       }
 
-      Axios.post('http://localhost:3001/api/updateuser', payload)
+      Axios.post(process.env.REACT_APP_ENDPOINT + '/api/updateuser', payload)
          .then(response => {
             getUsers();
             setSubmitted(false);
@@ -60,7 +60,7 @@ const Users = () => {
    }
 
    const deleteUser = (data) => {
-      Axios.post('http://localhost:3001/api/deleteuser', data)
+      Axios.post(process.env.REACT_APP_ENDPOINT + '/api/deleteuser', data)
          .then(response => {
             getUsers();
          })
